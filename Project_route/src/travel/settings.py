61651122a7b56882c14009b12ae4507544b5194d
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-30 14:24:29 +0300

    Project_route. Запуск нового проекта

--- Project_route/src/travel/settings.py
+++ Project_route/src/travel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&r=r!302#o8)c7pnev(_u_1$^yx-#g%q$yi3t@-xs#s!h#lj*l'
+SECRET_KEY = '4o!%5*2z_k%2r4v1m*=iiwfl^-77s*sqga(+x6&zy=w_o%!wjc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
